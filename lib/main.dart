c22195527b5683ab15f38fe5fa7d71d9f4aac2d6
sabbirahmed6amtech <syedsabbirahmed.6amtech@gmail.com>
2026-01-19 12:56:52 +0600

    fix : minor change in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Git Demo',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'This is for github practice'),
     );
   }
 }
